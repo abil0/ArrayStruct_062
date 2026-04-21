f58ef04d213e134e34cca523459c3bd93ac698ea
abil0 <hoenabil6@gmail.com>
2026-04-21 09:42:56 +0700

    add detail

--- struct2.cpp
+++ struct2.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat{
+    string Desa;
+    string Kota;
+};
+
 struct Mahasiswa 
 {
     string nama;
     string nim;
     string alamat;
-    string umur;
+    int umur;
 };
+
+int main()
+{
+    Mahasiswa mhs;
+
+    cout << "Nomer Mahasiswa= ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa = ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa = ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa = ";
+    cin >> mhs.umur;
+    
+
+
+    cout << endl;
+    cout << "Menampilkan Data" <<endl;
+    cout << endl;
+
+    cout << "\n NIM = " << mhs.nim << endl;
+    cout << "\n Nama = " << mhs.nama << endl;
+    cout << "\n Alamat = " << mhs.alamat << endl;
+    cout << "\n Umur = " << mhs.umur << endl;
+}   
