7030fa6f1ffabf859d9df5c4d2342d2208d939e5
abil0 <hoenabil6@gmail.com>
2026-04-21 09:53:27 +0700

    update part 2 struct 2

--- struct2.cpp
+++ struct2.cpp
@@ -24,7 +24,9 @@ int main()
     cin >> mhs.nama;
     cout << "Alamat Mahasiswa = ";
     cout << "\t Nama Desa: ";
-   
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
     cout << "Umur Mahasiswa = ";
     cin >> mhs.umur;
     
